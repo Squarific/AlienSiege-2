2977ac1176074ecb58f77d93c083c6779a7c46dc
= <=>
2016-08-26 17:07:04 +0200

    Fix menu rendering and syntax errors

--- src/cpp/view/Menu.cpp
+++ src/cpp/view/Menu.cpp
@@ -1,43 +1,64 @@
 #include "Menu.h"
 #include "Errors.h"
+#include "Observers.h"
 
 #include <SFML/Graphics.hpp>
 
-si::view::Menu () {
+#include <vector>
+#include <iostream>
+
+si::view::Menu::Menu () {
 	throw NotEnoughArgumentsError();
 }
 
-si::view::Menu (sf::RenderWindow* window) {
+si::view::Menu::Menu (sf::RenderWindow* window) {
 	this->window = window;
 
-	if (!this->font.loadFromFile("arial.ttf")) {
+	if (!this->font.loadFromFile("fonts/arial.ttf")) {
 		throw new FontFileException();
 	}
 }
 
-si::view::draw () {
+void si::view::Menu::draw () {
 	// Set this window as the active openGl window
 	this->window->setActive();
 	this->window->clear();
 
-	vector <std::string> options = {
+	std::vector <std::string> options = {
 		"Solo",
 		"Coop",
 		"Scores"
 	};
 
-	//center text
-	sf::FloatRect textBounds = text.getLocalBounds();
+	float startHeight = options.size() * 2.0f;
+	int counter = 0;
+	int textSize = 24;
+	for (auto& option : options) {
+		sf::Text text;
+		text.setString(option);
 
-	text.setOrigin(textBounds.left + textBounds.width / 2.0,
-	               textBounds.top  + textBounds.height / 2.0);
+		text.setCharacterSize(24);
+		text.setColor(sf::Color::White);
+		text.setStyle(sf::Text::Regular);
+		text.setFont(this->font);
 
-	text.setPosition(this->window->getSize() / 2);
+		//center text
+		sf::FloatRect textBounds = text.getLocalBounds();
+
+		text.setOrigin(textBounds.left + textBounds.width / 2.0f,
+		               textBounds.top  + textBounds.height / 2.0f);
+
+		text.setPosition(sf::Vector2f(this->window->getSize() / 2U) +
+		                 sf::Vector2f(0, -startHeight * textSize + 2 * textSize * counter));
+
+		this->window->draw(text);
+		counter++;
+	}
 
 	// End the frame and draw it all
 	this->window->display();
 }
 
-virtual void si::view::notify (si::Observable& subject, std::string type) {
+void si::view::Menu::notify (si::Observable& subject, std::string type) {
 
 }
