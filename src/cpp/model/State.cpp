82e808dadb620f663a5386e743730a092320fdfd
= <=>
2016-08-26 17:05:59 +0200

    Fix state syntax errors

--- src/cpp/model/State.cpp
+++ src/cpp/model/State.cpp
@@ -1,19 +1,20 @@
 #include "State.h"
+#include <iostream>
 
 si::model::State::State () {
 
 }
 
 bool si::model::State::inGame () {
-	return this->_state === std::string("game");
+	return this->_state == std::string("game");
 }
 
 bool si::model::State::scoreBoardOpen () {
-	return this->_state === std::string("scores");
+	return this->_state == std::string("scores");
 }
 
 bool si::model::State::inMenu () {
-	return this->_state === std::string("menu");
+	return this->_state == std::string("menu");
 }
 
 void si::model::State::setState (std::string state) {
